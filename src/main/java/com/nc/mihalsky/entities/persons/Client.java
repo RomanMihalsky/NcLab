7b07d809773d93deb42c5a45b57956e061a71735
Roman Mihalsky <roman.mi4alsky@gmail.com>
2020-10-22 15:27:35 +0300

    Add methods get/set and toString in class Client

--- src/main/java/com/nc/mihalsky/entities/persons/Client.java
+++ src/main/java/com/nc/mihalsky/entities/persons/Client.java
@@ -9,4 +9,80 @@ public class Client implements Person {
     private int phoneNumber;
     private int passportSeries;
     private int passportNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPassportSeries() {
+        return passportSeries;
+    }
+
+    public void setPassportSeries(int passportSeries) {
+        this.passportSeries = passportSeries;
+    }
+
+    public int getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(int passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", gender='" + gender + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
 }
