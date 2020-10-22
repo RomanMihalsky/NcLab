157a2194a163df7e4b711daf0d93f7f14159b724
Roman Mihalsky <roman.mi4alsky@gmail.com>
2020-10-22 15:24:57 +0300

    Add fields to Client

--- src/main/java/com/nc/mihalsky/entities/persons/Client.java
+++ src/main/java/com/nc/mihalsky/entities/persons/Client.java
@@ -1,4 +1,12 @@
 package com.nc.mihalsky.entities.persons;
 
 public class Client implements Person {
+    private  long id;
+    private String name;
+    private String surname;
+    private String patronymic;
+    private String gender;
+    private int phoneNumber;
+    private int passportSeries;
+    private int passportNumber;
 }
