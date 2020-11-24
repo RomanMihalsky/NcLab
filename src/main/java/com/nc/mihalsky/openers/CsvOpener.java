cc85119cc657d8d07c9dc1592941ba2efa147ff7
Роман Михальский <roman.mi4alsky@gmail.com>
2020-11-24 17:58:41 +0300

    added method readData to CsvOpner

--- src/main/java/com/nc/mihalsky/openers/CsvOpener.java
+++ src/main/java/com/nc/mihalsky/openers/CsvOpener.java
@@ -2,12 +2,37 @@ package com.nc.mihalsky.openers;
 
 import com.nc.mihalsky.entities.contracts.Contract;
 import com.nc.mihalsky.simple.SimpleListContract;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.FileReader;
+import java.util.List;
 
 public class CsvOpener implements Opener{
   public static final String CSV_FILE_PATH = "src\\main\\resources\\csvFile.csv";
+  private static List<String[]> allData;
 
   @Override
   public SimpleListContract<Contract> readFileToSimpleList(String fileName) {
+    readData(fileName);
+
     return null;
+  }
+
+  private void readData(String fileName) {
+    try {
+      FileReader fileReader = new FileReader(fileName);
+
+      CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build();
+
+      CSVReader csvReader = new CSVReaderBuilder(fileReader).withCSVParser(csvParser).build();
+      
+      allData = csvReader.readAll();
+
+    }catch (Exception e){
+      e.printStackTrace();
+    }
   }
 }
